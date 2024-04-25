36ba334be84de9d9184e9425fe9d8c935919deb1
Andrei Akentev <akentev2010@gmail.com>
2024-04-25 08:19:29 +0500

    add test for Task model

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -10,4 +10,16 @@ FactoryBot.define do
   sequence :avatar do |n|
     "https://taskmanager.com/assets/avatars/#{n}.jpg"
   end
+
+  sequence :task_name, aliases: [:name] do |n|
+    "Task test name #{n}"
+  end
+
+  sequence :task_description, aliases: [:description] do |n|
+    "Task test description #{n}"
+  end
+
+  sequence :task_expired_at, aliases: [:expired_at] do |n|
+    Date.today + n.days
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    task = create :task
+    task2 = create :task
+    puts "#{task.name}, #{task.description}, #{task.expired_at}"
+    puts "#{task2.name}, #{task2.description}, #{task2.expired_at}"
+    assert task.persisted?
+  end
 end
