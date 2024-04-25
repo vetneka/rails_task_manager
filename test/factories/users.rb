b838271b1abf6d64f6d65d37da8a5f36009f1694
Andrei Akentev <akentev2010@gmail.com>
2024-04-25 08:10:19 +0500

    add tests for Developer, Manager, Admin models

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -6,5 +6,17 @@ FactoryBot.define do
     email
     avatar
     type { "" }
+
+    factory :developer do
+      type { "Developer" }
+    end
+
+    factory :manager do
+      type { "Manager" }
+    end
+
+    factory :admin do
+      type { "Admin" }
+    end
   end
 end
