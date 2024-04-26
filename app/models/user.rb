2305984126cf2b001099450239e4bca249552de5
Andrei Akentev <akentev2010@gmail.com>
2024-04-26 08:44:47 +0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
 
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true, format: { with: /^([^@]*@){1}[^@]*$/,
+    message: "The email must contain the '@' symbol" }
 end
