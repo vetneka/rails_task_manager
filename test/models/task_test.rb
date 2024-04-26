e9a53eb5e4e267a3500148ad6493d0c98a33e6d1
Andrei Akentev <akentev2010@gmail.com>
2024-04-26 08:07:15 +0500

    CR. Delete console output

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -3,9 +3,6 @@ require 'test_helper'
 class TaskTest < ActiveSupport::TestCase
   test 'create' do
     task = create(:task)
-    task2 = create(:task)
-    puts "#{task.name}, #{task.description}, #{task.expired_at}"
-    puts "#{task2.name}, #{task2.description}, #{task2.expired_at}"
     assert task.persisted?
   end
 end
